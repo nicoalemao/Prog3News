a050a21f56cc70d1a619b408554df133fceb543b
nicoalemao <njayt2@gmail.com>
2023-04-25 22:09:16 -0300

    API noticias

---
+++ News/News/Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace News
+{
+    public static class Settings
+    {
+        public static string NewsApiKey
+        {
+            get { return "bf7f9b4198ad42b5836f5337f0366216"; }
+        }
+    }
+}
